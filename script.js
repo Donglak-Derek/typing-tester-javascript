a8015ed81a4c17484e60377c0f83885f91d91988
Derek <donglak.han@gmail.com>
2018-09-12 00:00:38 -0500

    detecting spelling errors

--- script.js
+++ script.js
@@ -7,33 +7,48 @@ const theTimer = document.querySelector(".timer");
 var timer = [0, 0, 0, 0];
 
 
-// Add Leading zero to numbers 9 or below (purely for aesthetics)
-
+// Add Leading zero to numbers 9 or below (purely for aesthetics) / make a num to string
+function leadingZero(time) {
+    if (time <= 9) {
+        time = "0" + time;
+    }
+    return time;
+}
 
 
 // Run a standard minute/second/hundredths timer:
 function runTimer() {
-    let currentTime = timer[0] + ":" + timer[1] + ":" + timer[2];
+    let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
     theTimer.innerHTML = currentTime;
     timer[3]++;
 
-    timer[0] = Math.floor((timer[3]/100)/60);
-    timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
-    timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+    timer[0] = Math.floor((timer[3]/100)/60); //min
+    timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60)); //sec
+    timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000)); //milli sec
 }
 
 
 // Match the text entered with the provided text on the page:
 function spellCheck() {
     let textEntered = testArea.value;
-    console.log(textEntered);
+    let originTextMatch = originText.substring(0, textEntered.length);//substring(starting position(included), ending position)
+
+    if (textEntered == originText) {
+        testWrapper.style.borderColor = "#429890"; // green
+    } else {
+        if (textEntered == originTextMatch) {
+            testWrapper.style.borderColor = "#65CCf3"; // blue
+        } else {
+            testWrapper.style.borderColor = "#E95D0F"; // orange
+        }
+    }
 }
 
 // Start the timer:
 function start() {
     let textEnteredLength = testArea.value.length;
     if (textEnteredLength === 0 ) {
-        setInterval(runTimer, 10);
+        setInterval(runTimer, 10);// setInterval(function, milliseconds);
     }
     console.log(textEnteredLength);
 }
